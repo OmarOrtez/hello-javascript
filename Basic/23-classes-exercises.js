8f496ab8a5f60e9b9375f98ff030af93f9ca60d5
OmarOrtez <alumno1085304@is-azarquiel.es>
2024-10-08 12:44:17 +0200

    ejercicios de clases hecho

--- Basic/23-classes-exercises.js
+++ Basic/23-classes-exercises.js
@@ -23,3 +23,101 @@ https://www.youtube.com/live/8p6SLAIgwZI?si=MS9o7qIhezx9NTQX&t=902
 // 9. Utiliza los get y set y muestra sus valores
 
 // 10. Sobrescribe un método de una clase que utilice herencia 
+
+class movil{
+    
+    constructor(marca,precio){
+        this.marca=marca
+        this.precio=precio
+    }
+    
+
+    descuento(precio){
+        let porcentaje=0
+        if(precio>=500){
+            porcentaje=precio-(precio*0.15)
+        }else{
+            porcentaje=precio
+            console.log('para un descuento el movil tiene que tener un valor mayor o igual a 500€')
+            return false
+        }
+        console.log(`el descuento es ${porcentaje}`)
+    }
+
+    static almacen(marca) {
+        switch (marca) {
+            case 'xiaomi':
+                console.log(true)
+                break;
+            case 'samsung':
+                console.log(true)
+                break;
+            case 'aphon':
+                console.log(true)
+                break;
+            case 'lenovo':
+                console.log(false)
+                break;
+
+            default:
+                console.log('no tenemos esa marca ')
+              break;
+        }
+    }
+
+}
+
+let movil1=new movil('xiaomi',485)
+console.log(movil1)
+movil1.descuento(movil1.precio)
+movil.almacen(movil1.marca)
+
+
+    class movilVendido extends movil{
+        #dni
+        #dirreccion
+        #contacto
+        #bank
+        constructor(marca,precio,dni,dirreccion,contacto,bank){
+            super(marca,precio)
+           this.#dni=dni
+            this.#dirreccion=dirreccion
+            this.#contacto=contacto
+            this.#bank=bank
+        }
+
+        get dirreccion(){
+            return this.#dirreccion
+        }
+
+        set bank(bank){
+            return this.#bank=bank
+        }
+        get bank(){
+            return this.#bank
+        }
+        descuento(precio) {
+            let porcentaje = 0;
+            if (precio >= 500) {
+                porcentaje = precio - (precio * 0.20);  // Mayor descuento para móviles vendidos
+                console.log(`Descuento especial para móviles vendidos. El precio con descuento es ${porcentaje}€`);
+            } else {
+                console.log('No aplica descuento para este móvil.');
+            }
+        }
+
+
+
+    }
+
+    let usuario=new movilVendido('xiaomi',500,'18548060D','calle alberche 10',665313400,'465372842')
+    usuario.bank=4653728222
+    console.log(usuario.bank)
+    console.log(usuario.marca)
+    usuario.descuento(usuario.precio)
+
+   
+
+
+
+
